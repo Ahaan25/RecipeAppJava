5f763544a5cf6df10d55e0bb46a0f8864f05043d
Ahaan25 <ahaan25092002@gmail.com>
2022-08-04 19:20:55 +0530

    Added basic functions

--- app/src/main/java/com/example/recipeappjava/CheckCourses.java
+++ app/src/main/java/com/example/recipeappjava/CheckCourses.java
@@ -2,7 +2,10 @@ package com.example.recipeappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -10,6 +13,7 @@ import org.w3c.dom.Text;
 public class CheckCourses extends AppCompatActivity {
 
     TextView appetizer, maincourse, dessert, palletecleanser;
+    ImageView app, mai, des, pal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,62 @@ public class CheckCourses extends AppCompatActivity {
         maincourse=findViewById(R.id.textView7);
         dessert=findViewById(R.id.textView8);
         palletecleanser=findViewById(R.id.textView9);
+        app=findViewById(R.id.imageView9);
+        mai=findViewById(R.id.imageView10);
+        des=findViewById(R.id.imageView11);
+        pal=findViewById(R.id.imageView12);
 
+        appetizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, Appetizer.class);
+            }
+        });
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, Appetizer.class);
+            }
+        });
 
+        maincourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, MainCourse.class);
+            }
+        });
+        mai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, MainCourse.class);
+            }
+        });
+
+        dessert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, Desserts.class);
+            }
+        });
+        des.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, Desserts.class);
+            }
+        });
+
+        palletecleanser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, PaleteCleansers.class);
+            }
+        });
+        pal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(CheckCourses.this, PaleteCleansers.class);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/recipeappjava/PaleteCleansers.java
@@ -0,0 +1,14 @@
+package com.example.recipeappjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaleteCleansers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palete_cleansers);
+    }
+}
