a46e859859a94947054ae50010b8df1478f36b4c
Ahaan25 <ahaan25092002@gmail.com>
2022-08-04 18:14:40 +0530

    Added basic functions

--- app/src/main/java/com/example/recipeappjava/CheckCuisines.java
+++ app/src/main/java/com/example/recipeappjava/CheckCuisines.java
@@ -3,12 +3,25 @@ package com.example.recipeappjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class CheckCuisines extends AppCompatActivity {
+
+    ImageView american, chinese, indian, italian, japanese, mexican;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_cuisines);
+
+        american=findViewById(R.id.imageView4);
+        chinese=findViewById(R.id.imageView3);
+        indian=findViewById(R.id.imageView5);
+        italian=findViewById(R.id.imageView6);
+        japanese=findViewById(R.id.imageView7);
+        mexican=findViewById(R.id.imageView8);
+
+
+
     }
 }
