c30e834ad3f8c1e80d5bf4d3c9288beec0444746
Ahaan25 <ahaan25092002@gmail.com>
2022-08-05 11:27:56 +0530

    Added basic functions

--- app/src/main/java/com/example/recipeappjava/Appetizer.java
+++ app/src/main/java/com/example/recipeappjava/Appetizer.java
@@ -3,12 +3,23 @@ package com.example.recipeappjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Appetizer extends AppCompatActivity {
+
+    ListView appetizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appetizer);
+
+        appetizer=findViewById(R.id.appetizerlist);
+
     }
 }
