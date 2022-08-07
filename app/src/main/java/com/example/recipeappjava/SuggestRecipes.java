7cef6ed1dc0210ccd4d3e66de14dd8177f21f2d0
Ahaan25 <ahaan25092002@gmail.com>
2022-08-07 15:31:08 +0530

    Added basic functions

--- app/src/main/java/com/example/recipeappjava/SuggestRecipes.java
+++ app/src/main/java/com/example/recipeappjava/SuggestRecipes.java
@@ -3,12 +3,45 @@ package com.example.recipeappjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SuggestRecipes extends AppCompatActivity {
+
+    Spinner selectcuisine, selectcourse;
+    EditText nameofdish;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggest_recipes);
+
+        selectcuisine=findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.SelectCuisines, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        selectcuisine.setAdapter(adapter);
+
+        selectcourse=findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this, R.array.SelectCourses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        selectcourse.setAdapter(adapter2);
+
+        nameofdish=findViewById(R.id.editTextTextPersonName);
+
+
+        submit=findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
