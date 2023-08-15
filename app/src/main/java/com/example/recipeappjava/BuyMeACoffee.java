4c490f84b3d5fc7c26c24b9b4ba9fe3c95564f3b
Ahaan25 <ahaan25092002@gmail.com>
2023-08-15 23:28:16 +0530

    Fixed UI and improved layouts

--- app/src/main/java/com/example/recipeappjava/BuyMeACoffee.java
+++ app/src/main/java/com/example/recipeappjava/BuyMeACoffee.java
@@ -7,9 +7,11 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,17 +19,22 @@ public class BuyMeACoffee extends AppCompatActivity {
 
     TextView paypal, paytm;
     Button dono;
+    EditText amt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_me_acoffee);
 
+        amt=findViewById(R.id.editTextTextPersonName2);
         dono=findViewById(R.id.button2);
         dono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                paymentUsingGooglePay();
+
+                int amount=Integer.parseInt(String.valueOf(amt.getText()));
+                Toast.makeText(BuyMeACoffee.this, "Amount payed: "+amount, Toast.LENGTH_SHORT).show();
+                //paymentUsingGooglePay();
             }
         });
 
@@ -56,6 +63,7 @@ public class BuyMeACoffee extends AppCompatActivity {
 
     }
 
+    /*
     private void paymentUsingGooglePay(){
         Uri uri=Uri.parse("upi://pay").buildUpon()
                 .appendQueryParameter("pa", "7038825627@paytm")
@@ -75,5 +83,6 @@ public class BuyMeACoffee extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    */
 
 }
--- app/src/main/java/com/example/recipeappjava/CheckCuisines.java
+++ app/src/main/java/com/example/recipeappjava/CheckCuisines.java
@@ -2,7 +2,9 @@ package com.example.recipeappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class CheckCuisines extends AppCompatActivity {
@@ -21,7 +23,53 @@ public class CheckCuisines extends AppCompatActivity {
         japanese=findViewById(R.id.imageView7);
         mexican=findViewById(R.id.imageView8);
 
+        american.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, AmericanCuisine.class);
+                startActivity(i);
+            }
+        });
 
+        chinese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, ChineseCuisine.class);
+                startActivity(i);
+            }
+        });
+
+        indian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, IndianCuisine.class);
+                startActivity(i);
+            }
+        });
+
+        italian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, ItalianCuisine.class);
+                startActivity(i);
+            }
+        });
+
+        japanese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, JapaneseCuisine.class);
+                startActivity(i);
+            }
+        });
+
+        mexican.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(CheckCuisines.this, MexicanCuisine.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
